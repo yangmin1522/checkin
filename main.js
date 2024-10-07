04409541e5f721f296352c7f8c04ad20ff57208f
YANG MIN <53432024+yangmin1522@users.noreply.github.com>
2024-10-07 08:26:43 +0500

    Update main.js
    delete Number()

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ const glados = async () => {
     return [
       'Checkin OK',
       `${checkin.message}`,
-      `Left Days ${Number(status.data.leftDays)}`,
+      `Left Days ${status.data.leftDays}`,
     ]
   } catch (error) {
     return [
